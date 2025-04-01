0f7250bb8842c117f08672d6c0b384af9ae97f56
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-04-02 01:50:34 +0900

    Add recipes for obtaining netherite scrap through crushing and nucleosynthesizing

--- server_scripts/extras/netherite_scrap.js
+++ server_scripts/extras/netherite_scrap.js
@@ -1,4 +1,5 @@
 ServerEvents.recipes((event) => {
+  // Create way to get netherite scrap
   event.remove({ type: "create:crushing", output: "create:cinder_flour" })
   event.custom({
     type: "create:crushing",
@@ -21,5 +22,80 @@ ServerEvents.recipes((event) => {
         item: "mekanism:dirty_netherite_scrap"
       },
     ]
+  })
+
+
+
+  // Mekanism way to get netherite scrap
+  event.custom({
+    type: "mekanism:nucleosynthesizing",
+    duration: 2000,
+    gasInput: {
+      amount: 16,
+      gas: "mekanism:antimatter"
+    },
+    itemInput: {
+      ingredient: {
+        item: "minecraft:nether_brick"
+      }
+    },
+    output: {
+      item: "minecraft:netherite_ingot"
+    }
+  })
+
+  event.custom({
+    type: "mekanism:nucleosynthesizing",
+    duration: 1500,
+    gasInput: {
+      amount: 12,
+      gas: "mekanism:antimatter"
+    },
+    itemInput: {
+      ingredient: {
+        item: "minecraft:polished_basalt"
+      }
+    },
+    output: {
+      item: "minecraft:ancient_debris"
+    }
+  })
+
+  // Or...
+  event.custom({
+    type: "mekanism:crushing",
+    input: {
+      ingredient: {
+        item: "minecraft:netherrack"
+      }
+    },
+    output: {
+      count: 1,
+      item: "create:cinder_flour"
+    }
+  })
+  event.custom({
+    type: "mekanism:crushing",
+    input: {
+      ingredient: {
+        item: "minecraft:cracked_nether_bricks"
+      }
+    },
+    output: {
+      count: 5,
+      item: "create:cinder_flour"
+    }
+  })
+  event.custom({
+    type: "mekanism:enriching",
+    input: {
+      amount: 64,
+      ingredient: {
+        item: "cratedelight:cinder_flour_bag"
+      }
+    },
+    output: {
+      item: "mekanism:dirty_netherite_scrap"
+    }
   })
 })
