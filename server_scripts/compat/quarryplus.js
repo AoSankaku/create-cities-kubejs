b8ae925ededfd07a4576e078f0fd8dfe73a8c909
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-04-26 22:11:20 +0900

    Add crafting recipe for repeat tick module using mechanical drill and iron ingots

---
+++ server_scripts/compat/quarryplus.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes((event) => {
+  event.custom({
+    type: "minecraft:crafting_shaped",
+    category: "misc",
+    key: {
+      a: "create:mechanical_drill",
+      i: "minecraft:iron_ingot",
+    },
+    pattern: [
+      "aaa",
+      "iii",
+      "iii"
+    ],
+    result: {
+      count: 1,
+      id: "quarryplus:repeat_tick_module"
+    }
+  })
+})
+
