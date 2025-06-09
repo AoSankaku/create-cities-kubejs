5bedb22fa4db640295b5663ced86fafbda5f01f5
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-06-10 00:13:31 +0900

    Remove deprecated XPBoostEnchantment initialization script

--- startup_scripts/compat/enchantments.js
+++
@@ -1,9 +0,0 @@
-/*
-StartupEvents.postInit(() => {
-  const $XPBoostEnchantment = Java.loadClass(
-    'com.enderio.base.common.enchantment.XPBoostEnchantment'
-  )
-
-  $XPBoostEnchantment.addValidItem('cataclysm:infernal_forge')
-})
-*/
