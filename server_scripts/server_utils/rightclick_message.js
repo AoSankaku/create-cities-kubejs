4476b05d6472dfe49c2498351de265500d658890
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-10-21 16:12:51 +0900

    Refactor rightclick_message.js to work properly

--- server_scripts/server_utils/rightclick_message.js
+++ server_scripts/server_utils/rightclick_message.js
@@ -1,4 +1,21 @@
+const rightClickableBlocksToShowMap = [
+  "mtr:station_name_entrance",
+  "mtr:route_sign_standing_light",
+  "mtr:route_sign_standing_metal",
+  "mtr:route_sign_wall_light",
+  "mtr:route_sign_wall_metal",
+  "mtr:station_name_tall_block",
+  "mtr:station_name_tall_block_double_sided",
+  "mtr:station_name_tall_wall",
+  "mtr:station_name_tall_standing",
+  "mtr:station_name_wall",
+  "mtr:station_name_wall_gray",
+  "mtr:station_name_wall_black"
+]
+
 BlockEvents.rightClicked(event => {
+  // console.log(Object.keys(event))
+  // console.log(event.hand.toString())
   const player = event.getPlayer();
   if (player == null) {
     return;
@@ -7,12 +24,11 @@ BlockEvents.rightClicked(event => {
   if (itemStack.id == "mtr:brush") {
     return;
   }
-  if (event.block != "mtr:station_name_entrance") {
-    return;
+  if (
+    rightClickableBlocksToShowMap.includes(event.block.getId())
+    && event.hand.toString() == 'MAIN_HAND'
+  ) {
+    const mtrMapMsg = Text.of("クリックでMTR鉄道マップを開く").red().bold().hover(urls.mtrMap).click(urls.mtrMap)
+    player.tell(mtrMapMsg)
   }
-  if (!event.hand == 'main_hand') {
-    return;
-  }
-  const mtrMapMsg = Text.of("クリックでMTR鉄道マップを開く").red().bold().hover(urls.mtrMap).click(urls.mtrMap)
-  player.tell(mtrMapMsg)
 })
